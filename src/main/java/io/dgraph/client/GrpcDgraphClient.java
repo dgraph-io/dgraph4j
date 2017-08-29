445611d0700fbe34cff52d2374ca3e772b417e19
Tomasz Zdybał <tzdybal@dgraph.io>
2017-08-29 22:25:10 +0200

    Simple request retry logic. Maven plugin enabled.

--- src/main/java/io/dgraph/client/GrpcDgraphClient.java
+++ src/main/java/io/dgraph/client/GrpcDgraphClient.java
@@ -35,49 +35,68 @@ import io.grpc.ManagedChannelBuilder;
  */
 public class GrpcDgraphClient implements DgraphClient {
 
-	private static final Logger logger = LoggerFactory.getLogger(GrpcDgraphClient.class);
+    private static final Logger logger = LoggerFactory.getLogger(GrpcDgraphClient.class);
 
-	private final ManagedChannel channel;
-	private final DgraphGrpc.DgraphBlockingStub blockingStub;
+    private final ManagedChannel channel;
+    private final DgraphGrpc.DgraphBlockingStub blockingStub;
+    public static final int MAX_MESSAGE_SIZE = 32 * 1024 * 1024;
 
-	private GrpcDgraphClient(final String theHostname, final int thePort) {
-		channel = ManagedChannelBuilder.forAddress(theHostname, thePort).usePlaintext(true).build();
-		blockingStub = DgraphGrpc.newBlockingStub(channel);
-	}
+    private GrpcDgraphClient(final String theHostname, final int thePort) {
+        channel = ManagedChannelBuilder.forAddress(theHostname, thePort).usePlaintext(true).build();
+        blockingStub = DgraphGrpc.newBlockingStub(channel).
+                withMaxInboundMessageSize(MAX_MESSAGE_SIZE).
+                withMaxOutboundMessageSize(MAX_MESSAGE_SIZE);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public GrpcDgraphResult query(final String theQuery) {
-		try {
-			logger.debug("Starting query...");
-			final Request aRequest = Request.newBuilder().setQuery(theQuery).build();
-			logger.debug("Sending request to Dgraph...");
-			final Response aResponse = blockingStub.run(aRequest);
-			logger.debug("Recevied response from Dgraph!!");
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GrpcDgraphResult query(final String theQuery) {
+        int cnt = 0;
+        while (true) {
+            try {
+                logger.debug("Starting query...");
+                final Request aRequest = Request.newBuilder().setQuery(theQuery).build();
+                logger.debug("Sending request to Dgraph...");
+                final Response aResponse = blockingStub.run(aRequest);
+                logger.debug("Recevied response from Dgraph!!");
 
-			logger.debug("Latency: " + aResponse.getL().toString());
-			return GrpcDgraphResult.newInstance(aResponse);
-		} catch (RuntimeException re) {
-			re.printStackTrace();
-		}
-		return null;
-	}
+                logger.debug("Latency: " + aResponse.getL().toString());
+                return GrpcDgraphResult.newInstance(aResponse);
+            } catch (RuntimeException re) {
+                logger.warn("Request error - retrying: " + re.getMessage());
+                sleep();
+                ++cnt;
+                if (cnt > 300) {
+                    break;
+                }
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void close() {
-		try {
-			channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
-		} catch (InterruptedException ie) {
-			throw new RuntimeException(ie);
-		}
-	}
+    private void sleep() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static DgraphClient newInstance(final String theHostname, final int thePort) {
-		return new GrpcDgraphClient(theHostname, thePort);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() {
+        try {
+            channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruptedException ie) {
+            throw new RuntimeException(ie);
+        }
+    }
+
+    public static DgraphClient newInstance(final String theHostname, final int thePort) {
+        return new GrpcDgraphClient(theHostname, thePort);
+    }
 }
