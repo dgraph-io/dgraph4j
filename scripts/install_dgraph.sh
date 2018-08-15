46274f5e02f2abafa1993d85925f8027d584ac25
Deepak Jois <deepak.jois@gmail.com>
2018-08-15 11:56:26 +0530

    Fix installation script for CI.
    Nightly builds don’t seem to be getting generated for a while.
    Modified script to use the latest Dgraph build instead.

--- scripts/install_dgraph.sh
+++ scripts/install_dgraph.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ -z "$TRAVIS_TAG" ]; then
-  curl https://get.dgraph.io -sSf | bash -s nightly
-else
-  curl https://get.dgraph.io -sSf | bash
-fi
+curl https://get.dgraph.io -sSf | bash
