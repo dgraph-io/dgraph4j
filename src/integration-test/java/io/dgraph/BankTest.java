e96a9fa40587e5bf5589ec366ecbde75839a1240
Lucas Wang <lucas@dgraph.io>
2018-10-25 16:52:37 -0700

    Changing the copyright notice to 2018 only

--- src/integration-test/java/io/dgraph/BankTest.java
+++ src/integration-test/java/io/dgraph/BankTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017-18 Dgraph Labs, Inc. and Contributors
+ * Copyright (C) 2018 Dgraph Labs, Inc. and Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
