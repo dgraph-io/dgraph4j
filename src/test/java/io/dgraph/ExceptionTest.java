201227b79edc09fa4676512b0a0bc02af0a8b286
ShivajiKharse <115525374+shivaji-dgraph@users.noreply.github.com>
2024-08-06 17:38:02 +0530

    remove unwanted test code (#221)

--- src/test/java/io/dgraph/ExceptionTest.java
+++ src/test/java/io/dgraph/ExceptionTest.java
@@ -1,9 +1,7 @@
 package io.dgraph;
 
-import com.google.protobuf.ByteString;
 import io.dgraph.DgraphProto.Mutation;
 import io.dgraph.DgraphProto.NQuad;
-import io.dgraph.DgraphProto.Response;
 import io.dgraph.DgraphProto.Value;
 import org.testng.annotations.Test;
 
@@ -38,43 +36,5 @@ public class ExceptionTest extends DgraphIntegrationTest {
   public void testReadOnlyException() {
     Transaction txn = dgraphClient.newReadOnlyTransaction();
     txn.mutate(mu);
-  }
-
-  @Test
-  public void testVectorSupport() {
-    String vect = "[1, 2, 3, 4, 5]";
-    NQuad quad1 =
-        NQuad.newBuilder()
-            .setSubject("0x1000")
-            .setPredicate("productV")
-            .setObjectValue(
-                Value.newBuilder().setVfloat32Val(ByteString.copyFromUtf8(vect)).build())
-            .build();
-
-    System.out.println("quad1: " + quad1.toString());
-    Mutation mu1 = Mutation.newBuilder().addSet(quad1).build();
-    Transaction txn = dgraphClient.newTransaction();
-    Response response1 = txn.mutate(mu1);
-    System.out.printf("response:---------------------------> " + response1.toString());
-    try {
-      txn.commit();
-    } catch (Exception e) {
-      System.out.printf("response------------exe: " + e.toString());
-    }
-
-    String query =
-        "{\n" + "  q(func: has(productV)) {\n" + "    uid\n" + "    productV\n" + "  }\n" + "}";
-
-    // Create a transaction
-    Transaction txn1 = dgraphClient.newTransaction();
-    try {
-      // Run the query
-      DgraphProto.Response response = txn1.query(query);
-
-      // Print the response JSON
-      System.out.println(response.getJson().toStringUtf8());
-    } catch (Exception e) {
-      System.out.println(e.toString());
-    }
   }
 }
